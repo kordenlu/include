2c89cf2e920845c23d252961e359300244401cd3
jimingmin <jimingmin@gmail.com>
2015-03-17 01:17:28 +0800

    增加若干key定义

--- cachekey_define.h
+++ cachekey_define.h
@@ -9,6 +9,11 @@
 #define CACHEKEY_DEFINE_H_
 
 #define RK_REGIST_PHONE_INFO			"regist_phone_info_"
+#define RK_REGIST_ADDR_INFO				"regist_addr_info_"
+#define RK_REGIST_ACCOUNT_INFO			"regist_account_info_"
+#define	RK_GOBAL_UIN					"gobal_uin"
+#define RK_USER_BASE_INFO				"user_base_info_"
+#define RK_ACCOUNT_NAME					"account_name_"
 
 
 #endif /* CACHEKEY_DEFINE_H_ */
